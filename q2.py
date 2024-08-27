bbf9a8bca5c5ab7d8e8a9a3a9ccf0011d748e29e
wulfiex <115159048+wulfiex@users.noreply.github.com>
2024-08-27 16:16:59 +0530

    Create q2.py

---
+++ q2.py
@@ -0,0 +1,10 @@
+str1= input("Enter the text for frequency counting")
+new_str = str1.split()
+print(new_str)
+freq=[]
+for word in new_str
+  if word in new_str
+freq[word]+=1
+  elif freq[word]=1
+print(freq)
+n=len(freq)
